4abdd0b8e1ab145fe6fd50e39dc442edd1accc26
Ian Davenport <g.chatzichristodoulou@outlook.com>
2021-01-25 14:41:59 +0000

    added listNotes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 
 
 const yargs = require("yargs")
-const {addNote} = require("../utils/node.js")
+const {addNote,ListNotes} = require("../utils/node.js")
+
 console.log(process.argv);
 console.log(yargs.argv);
 
-
+const command = process.argv[2]
 
 if (command="add"){ 
     console.log("adding note..");
     addNote(yargs.argv.note)
 }else if (command="list") {
     console.log("adding list...");
+    ListNotes();
 }else {
     console.log("command not found");
 }
 
-
+console.log(ListNotes);
--- utils/node.js
+++ utils/node.js
@@ -2,6 +2,17 @@ const addNote =( myNote) =>{
     console.log(myNote)
 }
 
+const ListNotes = () => {
+    const allNotes =loadNotes();
+    allNotes.map(note, index => {
+        console.log(note.reminder[index]);
+        console.log(`${index + 1}. ${note.reminder}`)
+    });
+ };
+
+ ListNotes()
+
 module.exports = {
-    addNote
+    addNote,
+    ListNotes
 }
